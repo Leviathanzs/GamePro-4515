f189bfb0599c50f8803be48919f85795f2ca6861
Leviathanzs <okywidhiansyah@gmail.com>
2022-09-21 11:15:28 +0700

    Add sc-02-03 translasiObjek

---
+++ Pertemuan 2/translasiObjek.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class translasiObjek : MonoBehaviour
+{
+    Vector3 kekanan;
+    Vector3 kekiri;
+    Vector3 keatas;
+    Vector3 kebawah;
+    // Start is called before the first frame update
+    void Start()
+    {
+        keatas = new Vector3(0,1,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + 
+        (keatas * 2f * Time.deltaTime);
+    }
+}
